064db50edbdf090fbc3d4c0d0654622a0e0cddb8
aggads <aggad.samy@gmail.com>
2018-11-14 15:52:41 +0100

    modif html

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Aggad Samy.", "A Designer.", "A Developer.", "Passionate."],
+      strings: ["Aggad Samy.", "test Designer.", "test Developer.", "Passionate."],
       typeSpeed: 100,
       loop: true,
     });
